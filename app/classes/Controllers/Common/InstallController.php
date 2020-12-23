95b9be903d72f25137a76c2789c27dbc5f5b09b9
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-12-23 11:12:46 +0200

    Added more users to InstallController, added media queries to CSS.

--- app/classes/Controllers/Common/InstallController.php
+++ app/classes/Controllers/Common/InstallController.php
@@ -14,9 +14,11 @@ class InstallController
         App::$db->createTable('users');
         App::$db->insertRow('users', ['name' => 'Virginija', 'surname' => 'Gaidiene', 'email' => 'user@user.com', 'phone' => 867915356, 'address' => 'Kelmo g. 13A', 'password' => 'user']);
         App::$db->insertRow('users', ['name' => 'Aleksandras', 'surname' => 'Colis', 'email' => 'user2@user.com', 'phone' => 869834764, 'address' => 'Zirmunu g. 15A', 'password' => 'user2']);
+        App::$db->insertRow('users', ['name' => 'Zenadija', 'surname' => 'Tomasevic', 'email' => 'user3@user.com', 'phone' => '', 'address' => '', 'password' => 'user3']);
         App::$db->createTable('feedback');
         App::$db->insertRow('feedback', ['user_id' => 0, 'date' => 1608710685, 'comment' => 'Blogiausias gym pasauly turbut']);
         App::$db->insertRow('feedback', ['user_id' => 1, 'date' => 1608710685, 'comment' => 'Susilauziau ranka ten su ju aparatura']);
+        App::$db->insertRow('feedback', ['user_id' => 2, 'date' => 1608710685, 'comment' => 'żaden polski trener, nie lubił']);
     }
 }
 
